c63c9387d8168c831b4007c15f8993ec1f92ade9
FilinMen <fiilinkadabra@tutamail.com>
2024-10-09 17:20:29 +0300

    sum

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 input_data = open("input.txt","r")
 data = input_data.read()
 a = int(data)
-b = 1
-s = int(((b + a)/2)*a)
+sum = 0
+for i in range(1,a + 1):
+    sum = sum + i
 output_data = open("output.txt","w")
-output_data.write(str(s))
-input_data.close()
+output_data.write(str(sum))
 output_data.close()
-"""
-for i in range(1,a,1):
-    print(i)
-    if i == 55:
-        break
-    print(i)
-"""
+input_data.close()
+    
 
 
 
