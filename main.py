1291f9cc1214d8b537770259fe86e4f8da02d55d
FilinMen <fiilinkadabra@tutamail.com>
2024-10-09 17:24:57 +0300

    end

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ for i in range(1,a + 1):
     sum = sum + i
 output_data = open("output.txt","w")
 output_data.write(str(sum))
-output_data.close()
 input_data.close()
+output_data.close()
+
     
 
 
