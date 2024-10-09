2f4f44d1f9547392a2218079bfccfa61f7afd5da
FilinMen <fiilinkadabra@tutamail.com>
2024-10-09 17:36:44 +0300

    good

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ input_data = open("input.txt","r")
 data = input_data.read()
 a = int(data)
 sum = 0
-for i in range(1,a + 1):
-    sum = sum + i
+if a >0:
+    for i in range(1,a + 1):
+        sum = sum + i
+else:
+    for i in range(a,2):
+        sum = sum + i
+b = str(sum)
 output_data = open("output.txt","w")
-output_data.write(str(sum))
+output_data.write(b)
 input_data.close()
 output_data.close()
 
